d0bc1fbe9170584043ab287de72fd9781b3ce3d7
Unknown <norman.denis@epitech.eu>
2017-10-02 11:26:52 +0200

    Xamarin Android Base

---
+++ JobTrends.Android.App/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace JobTrends.Android.App
+{
+	[Activity(Label = "JobTrends.Android.App", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
